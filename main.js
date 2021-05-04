f7eb5d069dbe96c3a64eda1347302499a76aac37
frontendpm <frontendpm@gmail.com>
2021-05-04 12:45:49 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+JS:
+1. you may use jQuery, but you don't have to
+2. {{socialNetworkName}} should be replaced with chosen social network name
+3. hash (#) should never be added to page's url
+4. bonus: clicking on the translucent background closes the dialog
+5. bonus: use Class
+*/
